e55b310f22e73e73722c90cd38614683302fded1
wlf <wendreolf@gmail.com>
2021-05-26 05:05:18 -0300

    1st steps...

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gallery/l10n/gallery_localizations.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class AppWidget extends StatelessWidget {
   @override
@@ -7,6 +10,17 @@ class AppWidget extends StatelessWidget {
     return MaterialApp(
       navigatorKey: Modular.navigatorKey,
       title: 'Flutter Slidy',
+      localizationsDelegates: [
+        GalleryLocalizations.delegate,
+        AppLocalizations.delegate, // Add this line
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+       supportedLocales: [
+        const Locale('en', ''), // English, no country code
+        //const Locale('es', ''), // Spanish, no country code
+      ],
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gallery/l10n/gallery_localizations.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:moneyManager/app/modules/home/teste.dart';
 import 'home_controller.dart';
+import 'package:flutter/cupertino.dart';
 
 class HomePage extends StatefulWidget {
   final String title;
@@ -15,12 +18,58 @@ class _HomePageState extends ModularState<HomePage, HomeController> {
 
   @override
   Widget build(BuildContext context) {
+    var localization = GalleryLocalizations.of(context);
+    final drawerHeader = UserAccountsDrawerHeader(
+      accountName: Text(
+        "usuário xyz",
+      ),
+      accountEmail: Text(
+        "teste@email.com",
+      ),
+      currentAccountPicture: const CircleAvatar(
+        child: FlutterLogo(size: 42.0),
+      ),
+    );
+    final drawerItems = ListView(
+      children: [
+        drawerHeader,
+        ListTile(
+          title: Text(
+            "Ir para página 1",
+          ),
+          leading: const Icon(Icons.favorite),
+          onTap: () {
+            Navigator.pop(context);
+          },
+        ),
+        ListTile(
+          title: Text(
+            "Ir para página 2",
+          ),
+          leading: const Icon(Icons.comment),
+          onTap: () {
+            Navigator.pop(context);
+          },
+        ),
+      ],
+    );
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(
+          "Texto",
+        ),
       ),
-      body: Column(
-        children: <Widget>[],
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          // child: Text(
+          //   localization.demoNavigationDrawerText,
+          // ),
+          child: DatumLegendWithMeasures.withSampleData(),
+        ),
+      ),
+      drawer: Drawer(
+        child: drawerItems,
       ),
     );
   }
