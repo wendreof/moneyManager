ee75a82c9e8d1726005badb9a90612500fe87214
wlf <wendreolf@gmail.com>
2021-05-25 23:59:01 -0300

    :1st

---
+++ lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:moneyManager/app/app_module.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+void main() => runApp(ModularApp(module: AppModule()));
